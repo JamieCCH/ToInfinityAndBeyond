fad11d9c02d8fd55b4b77e0258f07f343ce3c412
JamieCCH <jamie.cch@gmail.com>
2019-03-25 00:33:37 -0400

    Project init

---
+++ ToInfinityAndBeyond/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  ToInfinityAndBeyond
+//
+//  Created by Jamie on 2019/3/25.
+//  Copyright © 2019 Jamie. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+
+}
